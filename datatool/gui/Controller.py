ef035fb6bbc8b0fe1b9701ab3589ffa9a832a755
Vesiv <register626@gmail.com>
2018-11-02 00:43:57 -0400

    Implemented support for multiple tabs/sheets in gui, and began Find and Replace operation

--- datatool/gui/Controller.py
+++ datatool/gui/Controller.py
@@ -6,6 +6,8 @@ import pandas as pd
 from gui import Model
 import numpy as np
 import procedures
+import re
+import traceback
 
 # This is the controller
 
@@ -19,16 +21,17 @@ class MyWorkingCode(QtWidgets.QMainWindow, Ui_DataTool):
         # If a file was specified, load it up. If not, load empty.
         if filename != None:
             data = procedures.load(filename, path)
-            self.panda = Model.PandasTable(data)
+            panda = Model.PandasTable(data)
+            self.createTab(panda)
         else:
-            self.panda = Model.PandasTable(pd.DataFrame())
-
-        self.tableView.setModel(self.panda)
+            panda = Model.PandasTable(pd.DataFrame())
+            self.createTab(panda)
 
         # File Menu Actions
         self.actionExit.triggered.connect(self.Exit)
         self.actionLoad.triggered.connect(self.loadData)
         self.actionSave.triggered.connect(self.saveData)
+        self.actionOpen.triggered.connect(self.openData)
 
 
         # Edit Menu Actions
@@ -39,30 +42,147 @@ class MyWorkingCode(QtWidgets.QMainWindow, Ui_DataTool):
         self.actionRemove_Non_Numberic.triggered.connect(self.removeNonNumeric)
         self.actionCorrect_Date_Format.triggered.connect(self.correctDateFormat)
         self.actionDisperse_Ranks_By_Program.triggered.connect(self.ranksByProgram)
+        self.actionFind_and_Replace.triggered.connect(self.findAndReplace)
 
         # Run button
         self.runButton.clicked.connect(self.runCommand)
 
-
+    # Translates and runs commands entered through the text area
     def runCommand(self):
 
         command = self.textEdit.toPlainText()
 
-        self.panda.runCommand(command)
-        self.panda.layoutChanged.emit()
+        if command == '':
+            self.notifyUser("No command entered.")
+        else:
+
+            # Allows us to use df to specify the first tab's dataframe
+            command = re.sub( r'df(?!\d)' , 'self.getPanda(0).df', command)
+
+            # This allows us to use df2 df3 etc.. to refer to the proceding tabs' dataframes
+            match = re.findall(r'df\d', str(command))
+            if match is not None:
+                for x in match:
+                    num = int(re.search(r'\d', x)[0]) - 1
+                    command = command.replace( x , 'self.getPanda(' + str(num) + ').df')
+
+            try:
+                exec(command)
+                self.getCurrentPanda().layoutChanged.emit()
+            
+            # Lets user know they messed up and how
+            except:
+                errorMsg = 'There was a problem with the command entered. ' + \
+                           'See stack trace or show Scott: \n\n' + traceback.format_exc()
+                self.notifyUser(errorMsg)
+
+    # A simple message box to let the user know about something
+    def notifyUser(self, message):
+        msg = QtWidgets.QMessageBox()
+        msg.setText(message)
+        msg.exec()
+
+    #region Tab Functionality
+    # Gets the tableView currently in focus
+    def getCurrentView(self):
+
+        currentTab = self.tabWidget.currentWidget()
+        
+        View = currentTab.findChild(QtWidgets.QTableView)
+
+        return View
+
+    # Gets the PandaTable currently in focus
+    def getCurrentPanda(self):
+
+        view = self.getCurrentView()
+
+        if view != None:
+            panda = view.model()
+            return panda
+        else:
+            return None
+
+    # Gets the PandaTable at the given index
+    def getPanda(self, index):
+        tab = self.tabWidget.widget(index)
+        view = tab.findChild(QtWidgets.QTableView)
+        panda = view.model()
+        return panda
+
+    # Creates a new tab with the given PandaTable
+    def createTab(self, panda):
+
+        tab = QtWidgets.QWidget()
+        tab.setObjectName("tab")
+        verticalLayout = QtWidgets.QVBoxLayout(tab)
+        verticalLayout.setContentsMargins(0, 0, 0, 0)
+        verticalLayout.setSpacing(10)
+        verticalLayout.setObjectName("verticalLayout")
+        tableView = QtWidgets.QTableView(tab)
+        tableView.setSortingEnabled(False)
+        tableView.setObjectName("tableView")
+        tableView.verticalHeader().setDefaultSectionSize(30)
+        tableView.verticalHeader().setMinimumSectionSize(30)
+        verticalLayout.addWidget(tableView)
+        tableView.setModel(panda)
+        self.tabWidget.addTab(tab, "Tab")
+
+    # Deletes all tabs in the tabWidget
+    def deleteAllTabs(self):
+        
+        # Iterate through all tabs, removing each
+        for i in range(0,self.tabWidget.count()):
+            self.tabWidget.removeTab(i)
+
+        # For whatever reason, this one likes to stay, so we delete it explicitly
+        self.tabWidget.removeTab(self.tabWidget.currentIndex())
+
+    #endregion
 
     #region File Menu
     def loadData(self):     
 
         path = QtWidgets.QFileDialog.getOpenFileName(self, 'Open File', os.getenv('HOME'), 'CSV(*.csv)')
-        
-        self.panda.loadData(path)
-        self.panda.layoutChanged.emit()
+
+        # If a file was specified, load it up. If not, tell the user to pick a valid file
+        if path[0] != '':
+
+            if os.path.exists(path[0]) and os.path.getsize(path[0]):
+
+                self.OriginalData = pd.read_csv(path[0], index_col=None, dtype=object)
+
+                panda = Model.PandasTable(self.OriginalData)
+
+                self.deleteAllTabs()
+                self.createTab(panda)
+
+            else:
+                self.notifyUser("Please pick a valid file.")
+
+    def openData(self):
+
+        path = QtWidgets.QFileDialog.getOpenFileName(self, 'Open File', os.getenv('HOME'), 'CSV(*.csv)')
+
+        # If a file was specified, load it up. If not, tell the user to pick a valid file
+        if path[0] != None:
+
+            if os.path.exists(path[0]) and os.path.getsize(path[0]):
+
+                self.OriginalData = pd.read_csv(path[0], index_col=None, dtype=object)
+
+                panda = Model.PandasTable(self.OriginalData)
+
+                self.createTab(panda)
+
+            else:
+                self.notifyUser("Please pick a valid file.")
 
     def saveData(self):
 
         path = QtWidgets.QFileDialog.getSaveFileName(self, 'Save File', os.getenv('$HOME'))
-        self.panda.saveData(path)
+
+        self.getCurrentPanda().saveData(path)
 
     def Exit(self):
         QtCore.QCoreApplication.exit()
@@ -73,10 +193,10 @@ class MyWorkingCode(QtWidgets.QMainWindow, Ui_DataTool):
     def deleteData(self):
 
         # A selection model to get the current selection
-        selectionModel = self.tableView.selectionModel()
+        selectionModel = self.getCurrentView().selectionModel()
 
         # Pass it to the panda model to deal with
-        self.panda.deleteData(selectionModel)
+        self.getCurrentPanda().deleteData(selectionModel)
 
     #endregion
 
@@ -85,26 +205,26 @@ class MyWorkingCode(QtWidgets.QMainWindow, Ui_DataTool):
     def clearWhitespace(self):
 
         # A selection model to get the current selection
-        selectionModel = self.tableView.selectionModel()
+        selectionModel = self.getCurrentView().selectionModel()
 
         # Pass it to the panda model to deal with
-        self.panda.clearWhitespace(selectionModel)
+        self.getCurrentPanda().clearWhitespace(selectionModel)
 
     def removeNonNumeric(self):
 
         # A selection model to get the current selection
-        selectionModel = self.tableView.selectionModel()
+        selectionModel = self.getCurrentView().selectionModel()
 
         # Pass it to the panda model to deal with
-        self.panda.removeNonNumeric(selectionModel)
+        self.getCurrentPanda().removeNonNumeric(selectionModel)
 
     def correctDateFormat(self):
 
         # A selection model to get the current selection
-        selectionModel = self.tableView.selectionModel()
+        selectionModel = self.getCurrentView().selectionModel()
 
         # Pass it to the panda model to deal with
-        self.panda.correctDateFormat(selectionModel)
+        self.getCurrentPanda().correctDateFormat(selectionModel)
 
     def ranksByProgram(self):
 
@@ -112,7 +232,18 @@ class MyWorkingCode(QtWidgets.QMainWindow, Ui_DataTool):
         ranks, programs = Model.RanksByProgramsDialogBox.getResults(self.panda, self)
 
         # Passes values to PandaModel to be validated and acted upon
-        self.panda.ranksByPrograms(ranks, programs)
+        self.getCurrentPanda().ranksByPrograms(ranks, programs)
+
+    def findAndReplace(self):
+
+        # Prompts user for find regex and replace text
+        findText, replaceText = Model.FindAndReplaceDialogBox.getResults(self)
+        
+        # Gets the current selection from the current tab
+        selectionModel = self.getCurrentView().selectionModel()
+
+        # Pass to panda
+        self.getCurrentPanda().findAndReplace(findText, replaceText, selectionModel)
 
     #endregion
 
--- datatool/gui/Model.py
+++ datatool/gui/Model.py
@@ -10,7 +10,7 @@ import procedures
 
 class PandasTable(QtCore.QAbstractTableModel):
     #region Constructor
-    def __init__(self, data, parent=None):
+    def __init__(self, data, tabNumber=1, parent=None):
         QtCore.QAbstractTableModel.__init__(self, parent)
         self.df = data
 
@@ -56,36 +56,6 @@ class PandasTable(QtCore.QAbstractTableModel):
 
         self.df.to_csv(path[0], quoting=1, index=False)
 
-    def loadData(self, path):     
-
-        # Deals with invalid paths and user pressing ESC
-        if os.path.exists(path[0]) and os.path.getsize(path[0]):
-
-            # Tries to read the data, if it fails, tried with a more universal format
-            try:
-                self.OriginalData = pd.read_csv(path[0], index_col=None, dtype=object)
-
-            except:
-                self.OriginalData = pd.read_csv(path[0], index_col=None, dtype=object, encoding="ISO-8859-1" )
-
-            self.df = self.OriginalData
-
-        else:
-            msg = QtWidgets.QMessageBox()
-            msg.setText("No file selected.")
-            msg.exec()
-    
-    def runCommand(self, command):
-
-        if command == '':
-            msg = QtWidgets.QMessageBox()
-            msg.setText("No command entered.")
-            msg.exec()
-        else:
-            command = command.replace('df', 'self.df')
-            exec(command)
-    
-
     def deleteData(self, selectionModel):
 
         cols, rows = self.translateSelection(selectionModel)       
@@ -96,6 +66,7 @@ class PandasTable(QtCore.QAbstractTableModel):
             # python pandas method to drop the columns
             self.df = self.df.drop(cols, axis=1)
             self.layoutChanged.emit()
+            selectionModel.clear()
 
         # otherwise: check if rows are selected
         elif rows != None:
@@ -103,6 +74,7 @@ class PandasTable(QtCore.QAbstractTableModel):
             self.df = self.df.drop(rows)
             self.df = self.df.reset_index(drop=True)
             self.layoutChanged.emit()
+            selectionModel.clear()
         
         else:
             msg = QtWidgets.QMessageBox()
@@ -140,11 +112,13 @@ class PandasTable(QtCore.QAbstractTableModel):
         cols = self.translateSelection(selectionModel)       
         # If columns are selected, do the operation on each of them, 
         if cols != None:
-            for col in cols:
+            for col in cols[0]:
                 self.df = procedures.strip_whitespace(self.df, col)
         # otherwise: do it on the entire dataframe
         else:
             self.df = procedures.strip_whitespace(self.df)
+        
+        selectionModel.clear()
 
     def removeNonNumeric(self, selectionModel):
 
@@ -152,7 +126,7 @@ class PandasTable(QtCore.QAbstractTableModel):
 
         # If columns are selected, do the operation on each of them, 
         if cols != None:
-            for col in cols:
+            for col in cols[0]:
                 procedures.clean_phones(self.df, col)
         # otherwise: let the user know they need to select a column
         else:
@@ -160,19 +134,23 @@ class PandasTable(QtCore.QAbstractTableModel):
             msg.setText("No column selected.")
             msg.exec()
 
+        selectionModel.clear()
+
     def correctDateFormat(self, selectionModel):
         
         cols = self.translateSelection(selectionModel)
 
         # If columns are selected, do the operation on each of them, 
         if cols != None:
-            for col in cols:
+            for col in cols[0]:
                 procedures.fix_zp_dates(self.df, col)
         # otherwise: let the user know they need to select a column
         else:
             msg = QtWidgets.QMessageBox()
             msg.setText("No column selected.")
             msg.exec()
+
+        selectionModel.clear()
     
     def ranksByPrograms(self, ranks, programs):
 
@@ -188,6 +166,9 @@ class PandasTable(QtCore.QAbstractTableModel):
             procedures.fix_ranks(self.df, ranks, programs)
             self.layoutChanged.emit()
 
+    def findAndReplace(self, findText, replaceText, selectionModel):
+        #TODO: 
+        pass
 
 class RanksByProgramsDialogBox(QtWidgets.QDialog):
     
@@ -219,5 +200,34 @@ class RanksByProgramsDialogBox(QtWidgets.QDialog):
         result = dialog.exec_()
         if result == QtWidgets.QDialog.Accepted:
             return dialog.rankSelect.currentText(), dialog.programSelect.currentText()
+        else:
+            return None, None
+
+class FindAndReplaceDialogBox(QtWidgets.QDialog):
+    
+    def __init__(self, parent):
+        QtWidgets.QDialog.__init__(self)
+
+        self.findText = QtWidgets.QLineEdit()
+        self.replaceText = QtWidgets.QLineEdit()
+        self.layout = QtWidgets.QGridLayout(self)
+        self.layout.addWidget(self.findText, 0, 1)
+        self.layout.addWidget(QtWidgets.QLabel('Text to Find:'), 0, 0)
+        self.layout.addWidget(self.replaceText, 1, 1)
+        self.layout.addWidget(QtWidgets.QLabel('Text to Replace with:'), 1, 0)
+        self.confirm = QtWidgets.QPushButton('Confirm')
+        self.layout.addWidget(self.confirm, 3, 1)
+        self.confirm.clicked.connect(self.accept)
+        self.cancel = QtWidgets.QPushButton('Cancel')
+        self.cancel.clicked.connect(self.reject)
+        self.layout.addWidget(self.cancel, 3, 0)
+        self.setWindowTitle('Find and Replace')
+        self.setWindowModality(QtCore.Qt.ApplicationModal)
+
+    def getResults(self, parent=None):
+        dialog = FindAndReplaceDialogBox(parent)
+        result = dialog.exec_()
+        if result == QtWidgets.QDialog.Accepted:
+            return dialog.findText.text() , dialog.replaceText.text()
         else:
             return None, None
